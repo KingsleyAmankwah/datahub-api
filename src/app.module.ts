ca4c51d8f86687c76da541fcf5dd34de53f1ecf4
KingsleyAmankwah <kuamiklassic149@gmail.com>
2026-04-03 08:25:04 +0000

    feat: add database module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.env',
       cache: true,
     }),
+    DatabaseModule,
   ],
 })
 export class AppModule {}

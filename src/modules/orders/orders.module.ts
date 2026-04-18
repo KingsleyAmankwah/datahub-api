394ae7224ea9eed7a2c261e162a8e22f6dff1ad8
KingsleyAmankwah <kuamiklassic149@gmail.com>
2026-04-18 14:56:17 +0000

    refactor; update modules

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
+import { PaymentsModule } from '../payments/payments.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule, forwardRef(() => PaymentsModule)],
   providers: [OrdersService],
   exports: [OrdersService],
   controllers: [OrdersController],
--- src/modules/payments/payments.module.ts
+++ src/modules/payments/payments.module.ts
@@ -4,10 +4,16 @@ import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { MtnMomoProvider } from './providers/mtn-momo.provider';
 import { FulfillmentModule } from '../fulfillment/fulfillment.module';
+import { DevPaymentTestController } from './payments-dev.controller';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [OrdersModule, forwardRef(() => FulfillmentModule)],
-  controllers: [PaymentsController],
+  imports: [
+    forwardRef(() => OrdersModule),
+    forwardRef(() => FulfillmentModule),
+    UsersModule,
+  ],
+  controllers: [PaymentsController, DevPaymentTestController],
   providers: [PaymentsService, MtnMomoProvider],
   exports: [PaymentsService],
 })

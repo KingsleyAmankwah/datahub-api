87932337bb7a34d68f217ae2dfe01a42d94ef363
KingsleyAmankwah <kuamiklassic149@gmail.com>
2026-04-21 10:34:10 +0000

    refactor: add agents module to payments

--- src/modules/payments/payments.module.ts
+++ src/modules/payments/payments.module.ts
@@ -6,15 +6,17 @@ import { MtnMomoProvider } from './providers/mtn-momo.provider';
 import { FulfillmentModule } from '../fulfillment/fulfillment.module';
 import { DevPaymentTestController } from './payments-dev.controller';
 import { UsersModule } from '../users/users.module';
+import { AgentsModule } from '../agents/agents.module';
 
 @Module({
   imports: [
     forwardRef(() => OrdersModule),
     forwardRef(() => FulfillmentModule),
+    forwardRef(() => AgentsModule),
     UsersModule,
   ],
   controllers: [PaymentsController, DevPaymentTestController],
   providers: [PaymentsService, MtnMomoProvider],
-  exports: [PaymentsService],
+  exports: [PaymentsService, MtnMomoProvider],
 })
 export class PaymentsModule {}

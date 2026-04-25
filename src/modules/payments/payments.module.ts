b83edf64922512cf66624a983c81749e9c30e317
KingsleyAmankwah <kuamiklassic149@gmail.com>
2026-04-25 02:51:27 +0000

    feat: update payment provider with paystack

--- src/modules/payments/payments.module.ts
+++ src/modules/payments/payments.module.ts
@@ -2,7 +2,7 @@ import { forwardRef, Module } from '@nestjs/common';
 import { OrdersModule } from '../orders/orders.module';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
-import { MtnMomoProvider } from './providers/mtn-momo.provider';
+import { PaystackProvider } from './providers/paystack.provider';
 import { FulfillmentModule } from '../fulfillment/fulfillment.module';
 import { DevPaymentTestController } from './payments-dev.controller';
 import { UsersModule } from '../users/users.module';
@@ -16,7 +16,7 @@ import { AgentsModule } from '../agents/agents.module';
     UsersModule,
   ],
   controllers: [PaymentsController, DevPaymentTestController],
-  providers: [PaymentsService, MtnMomoProvider],
-  exports: [PaymentsService, MtnMomoProvider],
+  providers: [PaymentsService, PaystackProvider],
+  exports: [PaymentsService, PaystackProvider],
 })
 export class PaymentsModule {}

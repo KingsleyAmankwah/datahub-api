13cddaa815cd6dd547da0e6e79fda6413f224607
KingsleyAmankwah <kuamiklassic149@gmail.com>
2026-04-25 02:49:27 +0000

    refactor main file

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { rawBody: true });
   const config = app.get(ConfigService);
   const port = config.get<number>('PORT', 3000);
   const nodeEnv = config.get<string>('NODE_ENV', 'development');
